76c198042d8041556203671c33a133a0ec03f604
root <fjalvarez@gmail.com>
2014-01-23 17:52:33 +0100

    Display palettes

--- src/main/java/com/devaddiction/palettesearch/PaletteListAdapter.java
+++ src/main/java/com/devaddiction/palettesearch/PaletteListAdapter.java
@@ -1,11 +1,17 @@
 package com.devaddiction.palettesearch;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,15 +48,40 @@ public class PaletteListAdapter extends ArrayAdapter<Palette> {
 
             Palette palette = data.get(position);
 
-            TextView label = new TextView(context);
+/*            TextView label = new TextView(context);
             label.setText(palette.getTitle());
-            linearMain.addView(label);
+            linearMain.addView(label);*/
 
-            /*ImageView image = new ImageView(context);
-            image.setImageDrawable(palette.getImageUrl());
-            linearMain.addView(image);*/
+            ImageView image = new ImageView(context);
+            if (this.loadImageFromURL(palette.getImageUrl(), image)) {
+                linearMain.addView(image);
+            }
         }
 
         return row;
+    }
+
+    public boolean loadImageFromURL(String fileUrl,
+                                    ImageView iv){
+        try {
+
+            URL myFileUrl = new URL (fileUrl);
+            HttpURLConnection conn =
+                    (HttpURLConnection) myFileUrl.openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+
+            InputStream is = conn.getInputStream();
+            iv.setImageBitmap(BitmapFactory.decodeStream(is));
+
+            return true;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
